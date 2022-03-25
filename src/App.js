50fee3f322d5a263b1d622cd701343322ef0b48d
Gage Clancy <gclancy121@gmail.com>
2022-03-25 16:15:12 -0500

    oops i fixed everything in one commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Character from "./components/Character"
+import styled from 'styled-components';
+
+
 
 const App = () => {
+  const [isLoading, setLoading] =useState(true);
+  const [characterData, setCharacterData] = useState([]);
+  useEffect(() => {
+    axios.get(`https://swapi.dev/api/people/`)
+    .then (res => {
+      setCharacterData(res.data);
+      setLoading(false);
+    })
+
+  }, []);
+
+ 
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  if (isLoading) {
+    return <div className='App'>
+      <h1>Loading...</h1>
+      </div>;
+  }  else {
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+     {characterData.map(person => {
+      return <Character character={person}/>
+     })}
     </div>
   );
+  }
 }
 
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,31 @@
 // Write your Character component here
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledHolder = styled.div`
+background-color: gray;
+  display: flex;
+  margin-left: 15%;
+  flex-direction: column;
+  border: 1px solid white;
+  opacity: 75%;
+  width: 70%;
+`
+
+export default function Character(props) {
+    const {character} = props;
+    
+   
+return (
+    <StyledHolder className='character-holder'>
+        <h1>{character.name}</h1>
+            <div>
+                <p>Birth Year: {character.birth_year}</p>
+                <p>Eye Color: {character.eye_color}</p>
+                <p>Hair Color: {character.hair_color}</p>
+                <p>Films: {character.films.join(', ')} </p>
+            </div>
+    </StyledHolder>
+)
+
+}
